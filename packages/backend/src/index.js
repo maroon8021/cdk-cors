dfdfed8bf358c77545b2a53fe867cc631772a69d
maroon8021 <maroon8021@gmail.com>
2021-05-10 08:51:21 +0900

    Chore: CORS settings of apollo-server on lambda

--- packages/backend/src/index.js
+++ packages/backend/src/index.js
@@ -1,10 +1,67 @@
-exports.handler = async () => {
-  return {
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Headers": "Content-Type",
-      "Access-Control-Allow-Methods": "OPTIONS,GET,POST",
-    },
-    body: JSON.stringify("through-cors"),
+const express = require("express");
+const cors = require("cors");
+const { ApolloServer, gql } = require("apollo-server-express");
+const awsServerlessExpress = require("aws-serverless-express");
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => "Hello world!",
+  },
+};
+
+let cachedServer;
+function getServer() {
+  if (cachedServer) {
+    return cachedServer;
+  }
+
+  const app = express();
+
+  const corsOptions = {
+    origin: "http://example.com",
+    credential: true,
   };
+
+  app.use(cors(corsOptions));
+
+  const server = new ApolloServer({ typeDefs, resolvers });
+
+  server.applyMiddleware({
+    app,
+  });
+
+  cachedServer = app;
+  return cachedServer;
+}
+
+exports.handler = async (event, context) => {
+  console.log(event);
+
+  try {
+    const server = getServer();
+
+    const responseServer = awsServerlessExpress.createServer(
+      server,
+      undefined,
+      ["text/html"]
+    );
+
+    console.log("responseServer", responseServer);
+    const response = await awsServerlessExpress.proxy(
+      responseServer,
+      event,
+      context,
+      "PROMISE"
+    ).promise;
+    console.log("response", response);
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
 };
